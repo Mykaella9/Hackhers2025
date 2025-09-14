e461b46ba895faeaa8c5bf4434be5956c53cd536
dubuquee <dubuquee@msu.edu>
2025-09-14 17:23:04 -0400

    created skeleton banner

---
+++ src/components/WelcomeBanner.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function Header() {
+    return (
+        <div>
+            <header className="w-full py-8" style = {{backgroundColor: "#F5F5F5"}}>
+                <h1 className="text-3xl font-bold text-center" style = {{color: "#002D2A"}}>
+                    Welcome Crew
+                </h1>
+
+            </header>
+        </div>
+    )
+}
